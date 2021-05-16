a064a4c03ec0934e3524da4d1e0a8c6b5ef6cbdf
SKeerthi02 <keerthi.21094@gmail.com>
2021-05-16 11:52:41 -0400

    hands on-- setting class on span and button plus calling a function inside span

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 
 class CounterComponent extends Component {
+
+    state = {
+        count : 0
+    }
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
+                <span className="badge badge-primary badge-warning m-2">{this.getCount()}</span>
+                <button className = "btn btn-primary m-2">Increment</button>
             </div>
         );
+    }
+
+    getCount() {
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
     }
 }
 
