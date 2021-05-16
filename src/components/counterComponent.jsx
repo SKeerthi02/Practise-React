762501292c690b35298de0ea87a9e6e4c2eee502
SKeerthi02 <keerthi.21094@gmail.com>
2021-05-16 12:22:55 -0400

    Hands on -- Event handling and change the state of the variable using setState

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -6,12 +6,16 @@ class CounterComponent extends Component {
         count : 1,
         tags : ["tag1", "tag2", "tag3", "tag4"]
     }
+
+    handleIncrement = () => {
+        this.setState({count : this.state.count+ 1})
+}
     render() {
 
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.getCount()}</span>
-                <button className = "btn btn-secondary m-2">Increment</button>
+                <button onClick={this.handleIncrement} className = "btn btn-secondary m-2">Increment</button>
                 <ul>
                     {this.state.tags.map(tag => <li key ={tag}>{tag}</li>)}
                 </ul>
